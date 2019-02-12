eb80edce82f08b5dbf07442cbd78bb9905a97d16
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-02-12 16:41:13 +0100

    feat: add another test

--- test/index.ts
+++ test/index.ts
@@ -8,22 +8,36 @@ import {
 } from 'botbuilder-core'
 import { MultiTurnBot } from '../src/bot'
 
-!(async () => {
+function createAdapter() {
   const memoryStorage = new MemoryStorage()
   const conversationState = new ConversationState(memoryStorage)
   const userState = new UserState(memoryStorage)
   const bot = MultiTurnBot(conversationState, userState)
-  const adapter = new TestAdapter(bot.onTurn)
+  return new TestAdapter(bot.onTurn)
+}
 
-  await adapter
+!(async () => {
+  await createAdapter()
     .send('Hello')
     .assertReply("What's is your name?")
-    .send('Mr Robot')
+    .send('Mr. Robot')
     .assertReply('Do you want to give your age? (1) yes or (2) no')
     .send('yes')
     .assertReply('What is your age?')
     .send('42')
     .assertReply('I will remember that you are 42 years old.')
     .send('ok')
-    .assertReply('Your name is Mr Robot and you are 42 years old.')
+    .assertReply('Your name is Mr. Robot and you are 42 years old.')
+})()
+
+!(async () => {
+  await createAdapter()
+    .send('Hello')
+    .assertReply("What's is your name?")
+    .send('Mr. Robot')
+    .assertReply('Do you want to give your age? (1) yes or (2) no')
+    .send('no')
+    .assertReply('No age given.')
+    .send('ok')
+    .assertReply('Your name is Mr. Robot and you did not share your age.')
 })()
