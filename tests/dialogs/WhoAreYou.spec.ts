c1b76ddb5982e195c2d5257fc0bfcb64c3c64000
s.siefke.ext@bitworkers.eu <simon.siefke@gmail.com>
2019-02-22 12:48:59 +0100

    test: add another test for whoareyou dialog

--- tests/dialogs/WhoAreYou.spec.ts
+++ tests/dialogs/WhoAreYou.spec.ts
@@ -44,3 +44,17 @@ test('Remembers when an age is given', async () => {
     .send('42')
     .assertReply('I will remember that you are 42 years old.')
 })
+
+test('Prompts again for age it was invalid', async () => {
+  await createBot()
+    .send('Hello')
+    .assertReply("What's is your name?")
+    .send('Mr. Robot')
+    .assertReply('Do you want to give your age? (1) yes or (2) no')
+    .send('yes')
+    .assertReply('What is your age?')
+    .send('-1')
+    .assertReply("Your age can't be less than zero.")
+    .send('1')
+    .assertReply('I will remember that you are 1 years old.')
+})
